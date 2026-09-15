b10882c257994ea575303a95a7e46e0c9d1d8879
feytq <alexandrkharchenko987@gmail.com>
2026-09-15 11:09:22 +0700

    Добавить вывод маршрута для каждого алгоритма, исправить предыдущий цикл

--- lab1_kommivoyazher/main.cpp
+++ lab1_kommivoyazher/main.cpp
@@ -3,18 +3,18 @@
 #include "Matrix.h"
 #include "KommivoyazherSolver.h"
 
-void runExperiment(int nCities, int runIndex, int minRange, int maxRange, int startCity) {
+void runExperiment(int nCities, int runIndex, int minRange, int maxRange) {
     int** matrix = createMatrix(nCities);
     fillRandomMatrix(matrix, nCities, minRange, maxRange);
 
+    int startCity = 0;
     ExactResult exact = solveBruteForce(matrix, nCities, startCity);
     GreedyResult greedy = solveGreedy(matrix, nCities, startCity);
 
     double quality = calculateQuality(exact.minCost, exact.maxCost, greedy.cost);
 
     std::cout << "  Run #" << runIndex 
-              << " | Exact [Min: " << exact.minCost
-              << "Start City:" << startCity
+              << " | Exact [Min: " << exact.minCost 
               << ", Max: " << exact.maxCost 
               << ", Time: " << std::defaultfloat << exact.timeInSeconds << "s]"
               << " | Greedy [Cost: " << greedy.cost 
@@ -38,10 +38,9 @@ int main() {
     for (int i = 0; i < testSizesCount; i++) {
         int nCities = testSizes[i];
         std::cout << "\nDimension: " << nCities << "x" << nCities << std::endl;
-        for (int startCity = 0; startCity < testSizes[i]; startCity++){
-            for (int run = 1; run <= runsPerSize; run++) {
-                runExperiment(nCities, run, minCostRange, maxCostRange, startCity);
-            }
+        
+        for (int run = 1; run <= runsPerSize; run++) {
+            runExperiment(nCities, run, minCostRange, maxCostRange);
         }
     }
 
